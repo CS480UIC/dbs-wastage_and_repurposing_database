b76fbecf98842639d6ede720b6c91eec3b9e8e16
Joseph Lenaghan <77464979+JLen28@users.noreply.github.com>
2022-03-29 20:58:08 -0600

    Add files via upload

---
+++ sql/deleteDB.sql
@@ -0,0 +1,24 @@
+use wastage_and_repurposing;
+
+DELETE FROM waste;
+DROP TABLE waste;
+
+DELETE FROM waste_managment;
+DROP TABLE waste_managment;
+
+DELETE FROM waste_facility;
+DROP TABLE waste_facility;
+
+DELETE FROM county;
+DROP TABLE county;
+
+DELETE FROM state;
+DROP TABLE state;
+
+DELETE FROM users;
+DROP TABLE users;
+
+DELETE FROM entity1;
+DROP TABLE entity1;
+
+DROP DATABASE wastage_and_repurposing_database;
