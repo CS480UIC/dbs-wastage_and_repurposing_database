eef27c202a4ab29a45235b360d0d45f9493b908f
AlexVanBerkum1 <70489283+AlexVanBerkum1@users.noreply.github.com>
2022-03-31 21:08:28 -0500

    Update 7.2.3.sql

--- sql/queries.sql/7.2.3.sql
+++ sql/queries.sql/7.2.3.sql
@@ -2,10 +2,10 @@ CREATE
     ALGORITHM = UNDEFINED 
     DEFINER = `root`@`localhost` 
     SQL SECURITY DEFINER
-VIEW `7.2.3` AS
+VIEW `wastage_and_repurposing_database`.`7.2.3` AS
     SELECT 
-        `waste`.`countyid` AS `countyid`,
-        SUM(`waste`.`totalwaste`) AS `sum(totalwaste)`
+        `wastage_and_repurposing_database`.`waste`.`countyid` AS `countyid`,
+        SUM(`wastage_and_repurposing_database`.`waste`.`totalwaste`) AS `sum(totalwaste)`
     FROM
-        `waste`
-    GROUP BY `waste`.`countyid`
+        `wastage_and_repurposing_database`.`waste`
+    GROUP BY `wastage_and_repurposing_database`.`waste`.`countyid`
