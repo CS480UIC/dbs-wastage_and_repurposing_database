931743725aaa7f59732235d43dfc75473f64e52f
AlexVanBerkum1 <70489283+AlexVanBerkum1@users.noreply.github.com>
2022-03-31 21:11:12 -0500

    Update 7.3.1.sql

--- sql/queries.sql/7.3.1.sql
+++ sql/queries.sql/7.3.1.sql
@@ -2,10 +2,15 @@ CREATE
     ALGORITHM = UNDEFINED 
     DEFINER = `root`@`localhost` 
     SQL SECURITY DEFINER
-VIEW `wastage_and_repurposing_database`.`7.3.1` AS
+VIEW `wastage_and_repurposing_database`.`7.3.2` AS
     SELECT 
-        `wastage_and_repurposing_database`.`county`.`countyname` AS `countyname`,
-        `wastage_and_repurposing_database`.`waste`.`totalwaste` AS `totalwaste`
+        `wastage_and_repurposing_database`.`county`.`countyname` AS `countyname`
     FROM
-        (`wastage_and_repurposing_database`.`county`
-        JOIN `wastage_and_repurposing_database`.`waste` ON ((`wastage_and_repurposing_database`.`county`.`countyid` = `wastage_and_repurposing_database`.`waste`.`countyid`)))
+        `wastage_and_repurposing_database`.`county`
+    WHERE
+        (`wastage_and_repurposing_database`.`county`.`population` > (SELECT 
+                `wastage_and_repurposing_database`.`waste_management`.`totalwaste`
+            FROM
+                `wastage_and_repurposing_database`.`waste_management`
+            WHERE
+                (`wastage_and_repurposing_database`.`waste_management`.`facilityid` = 1)))
