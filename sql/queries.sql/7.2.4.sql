a36e6353c035d9f166895e31529922413d75247f
AlexVanBerkum1 <70489283+AlexVanBerkum1@users.noreply.github.com>
2022-03-31 21:08:44 -0500

    Update 7.2.4.sql

--- sql/queries.sql/7.2.4.sql
+++ sql/queries.sql/7.2.4.sql
@@ -2,10 +2,10 @@ CREATE
     ALGORITHM = UNDEFINED 
     DEFINER = `root`@`localhost` 
     SQL SECURITY DEFINER
-VIEW `7.2.4` AS
+VIEW `wastage_and_repurposing_database`.`7.2.4` AS
     SELECT 
-        `waste`.`countyid` AS `countyid`,
-        `waste`.`totalwaste` AS `totalwaste`
+        AVG(`wastage_and_repurposing_database`.`waste`.`plastic`) AS `plasticAVG`,
+        `wastage_and_repurposing_database`.`waste`.`totalwaste` AS `totalwaste`
     FROM
-        `waste`
-    GROUP BY `waste`.`countyid`
+        `wastage_and_repurposing_database`.`waste`
+    GROUP BY `wastage_and_repurposing_database`.`waste`.`totalwaste`
