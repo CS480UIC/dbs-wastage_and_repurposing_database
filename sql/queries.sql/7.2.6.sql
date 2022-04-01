c2b9fe0450c89547f8161b11b8f4fd8383c0937d
AlexVanBerkum1 <70489283+AlexVanBerkum1@users.noreply.github.com>
2022-03-31 21:10:15 -0500

    Update 7.2.6.sql

--- sql/queries.sql/7.2.6.sql
+++ sql/queries.sql/7.2.6.sql
@@ -2,11 +2,11 @@ CREATE
     ALGORITHM = UNDEFINED 
     DEFINER = `root`@`localhost` 
     SQL SECURITY DEFINER
-VIEW `7.2.6` AS
+VIEW `wastage_and_repurposing_database`.`7.2.6` AS
     SELECT 
-        `county`.`countyname` AS `countyname`,
-        `county`.`stateid` AS `stateid`
+        CONCAT('Chill',
+                `wastage_and_repurposing_database`.`state`.`statename`) AS `CONCAT('Chill',statename)`
     FROM
-        `county`
-    GROUP BY `county`.`countyname`
-    HAVING (AVG(`county`.`population`) > 15000)
+        `wastage_and_repurposing_database`.`state`
+    WHERE
+        (`wastage_and_repurposing_database`.`state`.`stateid` = 1)
