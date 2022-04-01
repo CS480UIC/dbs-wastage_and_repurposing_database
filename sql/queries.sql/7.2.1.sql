580d1b5be20689387f8083a8bdaeea6476a640a5
AlexVanBerkum1 <70489283+AlexVanBerkum1@users.noreply.github.com>
2022-03-31 21:07:54 -0500

    Update 7.2.1.sql

--- sql/queries.sql/7.2.1.sql
+++ sql/queries.sql/7.2.1.sql
@@ -2,8 +2,8 @@ CREATE
     ALGORITHM = UNDEFINED 
     DEFINER = `root`@`localhost` 
     SQL SECURITY DEFINER
-VIEW `7.2.1` AS
+VIEW `wastage_and_repurposing_database`.`7.2.1` AS
     SELECT 
-        SQRT(`waste`.`totalwaste`) AS `SQRT(totalwaste)`
+        SQRT(`wastage_and_repurposing_database`.`waste`.`totalwaste`) AS `SQRT(totalwaste)`
     FROM
-        `waste`
+        `wastage_and_repurposing_database`.`waste`
